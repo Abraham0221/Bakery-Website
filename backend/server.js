2e6e0e84289c0b3228fec8f1a298265b08732f95
ulysses <ulyssesgutierrez0221@gmail.com>
2025-06-24 22:06:38 -0500

    added teh server.js part of the backend

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const http = require('http')
+const fs = require('fs')
+const url = require('url')
+const querystring = require('querystring')
+
+const server = http.createServer((req, res) =>{
+    const page = url.parse(req.url).pathname;
+    const params = querystring.parse(url.parse(req.url).query);
+    console.log(page);
+    if (page == '/') {
+        fs.readFile('index.html', function(err, data) {
+            res.writeH (200, {'Content-Type': 'text/html'})
+            res.write(data)
+            res.end();
+        });
+    }
+    
+})
